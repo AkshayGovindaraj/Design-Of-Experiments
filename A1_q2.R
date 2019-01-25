facf83dfd18ee7dffe9ef1d2a5e0ced471649b83
Akshay Govindarj <akshaygvdrj@gmail.com>
2019-01-25 10:04:09 -0500

    Assignment1
    Final submission done

--- A1_q2.R
+++ A1_q2.R
@@ -1,12 +1,24 @@
 # Question 2 Studying Crime data
-
+library(leaps)
 crime_data = read.table("Ericksen.dat.txt", header = TRUE)
 
 # Part a : Regression with all variables
 
 model = lm(Undercount ~ Minority + Crime + Poverty + Language + HighSchool + Housing + Conventional, data=crime_data)
-
 summary(model)
 
 # Residual Plot
 plot(model$fitted.values, model$residuals)
+
+# City variable - Undercount difference
+plot(crime_data$City,crime_data$Undercount, main="Undercount variation between Cities and Countryside")
+
+# Part c : Best Subsets model
+regsubsets.out <-
+  regsubsets(bwt ~ age + lwt + race.cat + smoke + preterm + ht + ui + ftv.cat,
+             data = lbw,
+             nbest = 1,       # 1 best model for each number of predictors
+             nvmax = NULL,    # NULL for no limit on number of variables
+             force.in = NULL, force.out = NULL,
+             method = "exhaustive")
+regsubsets.out
--- A1_q3.R
+++ A1_q3.R
@@ -5,6 +5,15 @@ colnames(car_data)[1] <- "Toyota"
 colnames(car_data)[2] <- "Ford"
 colnames(car_data)[3] <- "GM"
 
-model = lm(GM ~ Toyota + Ford, data=car_data)
+#Scatterplots
+par(mfrow=c(1,2))
+plot(car_data$Toyota,car_data$GM)
+plot(car_data$Ford,car_data$GM)
 
+#Regression Model
+model = lm(GM ~ Toyota + Ford, data=car_data)
 summary(model)
+
+#Residual Plot
+par(mfrow=c(1,1))
+plot(model$fitted.values, model$residuals)
